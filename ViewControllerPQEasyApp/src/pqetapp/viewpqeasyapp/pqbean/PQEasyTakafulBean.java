8959a3442279c8aa4f5d3f3bd2bcffb063135eae
Farrukh Shaikh <52906883+FarrukhShaikh21@users.noreply.github.com>
2022-05-27 12:47:20 +0500

    ADDING CLASSESS INITIAL NEW FOLDER

---
+++ ViewControllerPQEasyApp/src/pqetapp/viewpqeasyapp/pqbean/PQEasyTakafulBean.java
@@ -0,0 +1,29 @@
+package pqetapp.viewpqeasyapp.pqbean;
+
+import javax.faces.context.ExternalContext;
+
+import javax.faces.context.FacesContext;
+
+import javax.servlet.http.HttpSession;
+
+import oracle.adf.share.ADFContext;
+
+public class PQEasyTakafulBean {
+    public PQEasyTakafulBean() {
+        super();
+    }
+    String strUserName;
+
+    public void setStrUserName(String strUserName) {
+        this.strUserName = strUserName;
+    }
+
+    public String getStrUserName() {
+        return strUserName;
+    }
+    
+    public String doLoginPQEashApp() {
+        ADFContext.getCurrent().getPageFlowScope().put("pCNICNo",getStrUserName());
+        return null;
+    }
+}
